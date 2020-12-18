9677557236034aa92c5d109e22076dbd7f0ac179
1795661808 <69446758+1795661808@users.noreply.github.com>
2020-12-18 17:46:27 +0800

    Add files via upload

---
+++ 输入n，求和1到n，要求不能使用乘除法，不使用任何if while for 以及三目运算，怎么做？.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include<string.h>
+
+/*输入n，求和1到n，要求不能使用乘除法，不使用任何if while for 以及三目运算，怎么做？
+*/
+
+ int f(int n) 
+{
+	n && (n += f(n - 1));
+	return n;
+}
+int main(void) 
+{
+	int n = 0;
+
+	scanf_s("%d", &n);
+
+	printf("%d\n", f(n));
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int sum(int n);
+//int resum = 0;
+//int sum(int n) 
+//{
+//
+//    return n && (resum += n) && sum(n - 1);
+//
+//}
+//int main(void)
+//{
+//    int x;
+//
+//    scanf_s("%d", &x);
+//
+//    printf("%d", sum(x));
+//
+//    
+//
+//
+//
+//}
